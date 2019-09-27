1a98746fdabc0062f38888b9a957f78e4d5dc32a
vizoR1986 <vizorka1986@gmail.com>
2019-09-27 10:52:16 +0200

    fix syntax issues with groovy pipeline

--- buildPipeline.groovy
+++ buildPipeline.groovy
@@ -2,23 +2,31 @@
 
 pipeline {
 
-  node any
+  agent master
 
   stages {
       stage ('build docker image') {
-          sh "docker build ./ -t yaml-cpp:0.1"
+          steps {
+            sh 'docker build ./ -t yaml-cpp:0.1'
+        }
       }
 
       stage ('execute the docker image') {
-          sh "docker run -itd --memory=1024m --cpus=1 yaml-cpp:0.1"
+          steps {
+            sh 'docker run -itd --memory=1024m --cpus=1 yaml-cpp:0.1'
+        }
       }
 
       stage ('get the archived binaries') {
-          sh './gatherOutput.sh'
+          strps{
+            sh './gatherOutput.sh'
+          }
       }
 
       stage ('Upload archive') {
-          archiveArtifacts artifacts: 'buildOutput/*.tar.gz'
+          steps {
+            archiveArtifacts artifacts: 'buildOutput/*.tar.gz'
       }
+    }
   }
 }
