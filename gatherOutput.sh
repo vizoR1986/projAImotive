11bf946876d081ac146daa7f9bb9d641bf95c0ac
vizoR1986 <vizorka1986@gmail.com>
2019-09-27 11:15:03 +0200

    Added name tag for container creation

--- gatherOutput.sh
+++ gatherOutput.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+DOCKERNAME="archiveLib"
 
 #Run the docker image
-docker run -itd --memory=1024m --cpus=1 yaml-cpp:0.1
+docker run -itd --name $DOCKERNAME --memory=1024m --cpus=1 yaml-cpp:0.1
 
 #Copy the generated tar.gz
-docker cp `docker ps -qa`:/buildOutput/ ./
+docker cp $DOCKERNAME:/buildOutput/ ./
 
 #destroy the docker container and remove the generated image
 docker rm -f `docker ps -qa`
